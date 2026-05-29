78ad753c3cf029ec6bdc0e89def650f2e5e63e95
Joseph <joseph010531@gmail.com>
2026-05-29 17:56:30 +0800

    learn python

---
+++ src/basics.py
@@ -0,0 +1,19 @@
+
+#variable
+year = 18
+
+#list
+buy_list = ['milk', 'egg', 'bread']
+
+#dict
+
+
+
+student_dict = {
+    'name': 'John',
+    'age': 18
+}
+
+#function
+def add(a,b):
+    return a + b
